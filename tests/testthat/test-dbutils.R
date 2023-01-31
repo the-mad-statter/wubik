30c5b7b2d263df0d61a817019ec227152efc29ab
Matthew Schuelke <matt@themadstatter.com>
2023-01-31 11:45:40 -0600

    update test

--- tests/testthat/test-dbutils.R
+++ tests/testthat/test-dbutils.R
@@ -1,3 +1,3 @@
 test_that("ephemeral path correct", {
-  expect_equal(dbutils.rlib.ephemeral_path(), "file:/usr/lib/R/library")
+  expect_equal(dbutils.rlib.ephemeralpath(), "file:/usr/lib/R/library")
 })
